bc920ed8a6d9f697cdd3efe4c8815d7dd742ea44
Alexey <108893923+Alexey913@users.noreply.github.com>
2023-06-12 17:13:37 +0300

    Update task_6.py

--- les_1/task_6.py
+++ les_1/task_6.py
@@ -6,12 +6,16 @@
 
 
 START_GRIGORIAN_CALENDAR = 1582
+MULTIPLY_4 = 4
+MULTIPLY_100 = 100
+MULTIPLY_400 = 400
 
 year = int(input('Введите год в формате yyyy: '))
 leap_year = None
 if year < START_GRIGORIAN_CALENDAR:
     leap_year = "Год был до принятия Григорианского календаря!"
-elif year % 4 != 0 or (year % 100 == 0 and year % 400 != 0):
+elif year % MULTIPLY_4 != 0 or \
+     year % MULTIPLY_100 == 0 and year % MULTIPLY_400 != 0:
     leap_year = "Год не является високосным!"
 else:
     leap_year = "Год является високосным!"
