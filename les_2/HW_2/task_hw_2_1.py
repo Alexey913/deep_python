70bd9b806e25982007938156dbd8c7e162cdddef
Alexey <108893923+Alexey913@users.noreply.github.com>
2023-06-15 11:12:32 +0300

    Update les_2/HW_2/task_hw_2_1.py
    Co-authored-by: Danil <49741194+samodurOFF@users.noreply.github.com>

--- les_2/HW_2/task_hw_2_1.py
+++ les_2/HW_2/task_hw_2_1.py
@@ -9,12 +9,8 @@ BASE = 16
 def convert(input_number: int) -> str:
     output_number = []
     translate_to_hex = '0123456789abcdef'
-    while input_number > 0:
-        delimeter = input_number % BASE
-        for keys_for_hex in translate_to_hex:
-            if delimeter == keys_for_hex:
-                delimeter = translate_to_hex.get(keys_for_hex)
-        output_number.insert(0, str(delimeter))
+    while input_number:
+        output_number.insert(0, translate_to_hex[input_number % BASE])
         input_number //= BASE
     return ''.join(output_number)
 
