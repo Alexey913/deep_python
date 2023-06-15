7774975fc696bb73668875221b39d01c0d9b7121
Alexey <108893923+Alexey913@users.noreply.github.com>
2023-06-15 17:03:40 +0300

    Update task_hw_3_3.py

--- les_3/HW_3/task_hw_3_3.py
+++ les_3/HW_3/task_hw_3_3.py
@@ -2,14 +2,42 @@
 # ключа и их массой в качестве значения. Определите какие 
 # вещи влезут в рюкзак передав его максимальную 
 # грузоподъёмность. Достаточно вернуть один допустимый вариант.
+# *Верните все возможные варианты комплектации рюкзака.
+def common_sum (dict_of_items, set_of_items):
+    sum = 0
+    for key, value in dict_of_items.items():
+        if not key in set_of_items:
+            sum+=value
+    return sum
+    
+    
+def find_max (temp_dict, set_of_items, list_fail):
+    while temp_dict:
+        max_value = max(temp_dict, key=(lambda k: dict_of_items[k]))
+        if max_value in set_of_items or max_value in list_fail:
+            temp_dict.pop(max_value)
+        else:
+            list_fail.append(max_value)
+            break
+    return max_value
 
-dict_of_items = dict(Котелок=3, Палатка=4, Гитара=2, Мангал=1, Шампура=0.3, Тушенка=1, Мясо=5, Вода=5, Сухпаек=3, Спички=0.1)
 
-size_bag = int("Введите грузоподъемность рюкзака: ")
+dict_of_items = dict\
+(Котелок=3, Палатка=4.1, Гитара=2.7, Мангал=2, Шампура=0.3, Тушенка=0.8, Мясо=5, Вода=9, Сухпаек=3, Спички=0.1)
 
-def items_count (dict_item, size):
-    list_of_items = []
-    if size == 0:
-        return list_of_items
-    for item, weigth in dict_item:
-        if 
+set_of_items = set()
+list_fail = []
+size_bag = float(input("Введите грузоподъемность рюкзака в кг: "))
+if common_sum(dict_of_items, set_of_items) <= size_bag:
+    for k in dict_of_items:
+        set_of_items.add(k)
+else:
+    while size_bag >= min(dict_of_items.values()):
+        temp_dict = dict_of_items.copy()
+        current_item = find_max(temp_dict, set_of_items, list_fail)
+        if dict_of_items[current_item] <= size_bag:
+            set_of_items.add(current_item)
+            size_bag-=dict_of_items[current_item]
+        else:
+            list_fail.append(current_item)
+print("Из имеющегося набора можно взять -", set_of_items)
