8f946cd263df60c19d038ba1d41df3dca36e61dd
Alexey <al913@mail.ru>
2023-07-23 22:19:19 +0300

    Lesson 14

--- les_14/test_5.py
+++ les_14/test_5.py
@@ -3,7 +3,17 @@
 # а также вычисляющую периметр, площадь и позволяющий складывать и вычитать прямоугольники беря за основу периметр. 
 # Напишите 3-7 тестов unittest для данного класса.
 
-from les_11.Task_6_Reactangle import Rectangle
+import unittest
 
+from Rectangle_descriptor import Rectangle
 
-Rec = Rectangle(5,2)
+class TestRectangle(unittest.TestCase):
+
+    def setUp(self) -> None:
+        for_create_test = Rectangle(2,5)
+
+    def test_no_change(self):
+        self.assertEqual(for_create_test, msg='Тест на создание прямоугольника провален!')
+
+if __name__ == '__main__':
+    unittest.main()
